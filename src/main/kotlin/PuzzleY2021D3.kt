a772000c8a5a9a656640d9a0433baca737c27a09
Chris Dziemborowicz <chris@dziemborowicz.com>
2022-12-01 00:17:57 -0800

    Add Y2021D3 solution.

---
+++ src/main/kotlin/PuzzleY2021D3.kt
@@ -0,0 +1,55 @@
+class PuzzleY2021D3 : Puzzle {
+
+  private lateinit var report: List<String>
+
+  private val bitLength: Int
+    get() = report[0].length
+
+  override fun parse(input: String) {
+    report = input.parseStrings()
+  }
+
+  override fun solve1(): Int {
+    var gammaRate = ""
+    var epsilonRate = ""
+    for (index in 0 until bitLength) {
+      if (mostCommonChar(report, index) == '0') {
+        gammaRate += "0"
+        epsilonRate += "1"
+      } else {
+        gammaRate += "1"
+        epsilonRate += "0"
+      }
+    }
+    return gammaRate.toInt(2) * epsilonRate.toInt(2)
+  }
+
+  override fun solve2(): Int {
+    val oxygenGeneratorRating = findRatingBy(::mostCommonChar)
+    val co2ScrubberRating = findRatingBy(::leastCommonChar)
+    return oxygenGeneratorRating * co2ScrubberRating
+  }
+
+  private fun findRatingBy(requiredBitProvider: (List<String>, Int) -> Char): Int {
+    val candidates = report.toMutableList()
+    for (index in 0 until bitLength) {
+      if (candidates.size == 1) break
+      val requiredBit = requiredBitProvider(candidates, index)
+      candidates.removeIf { it[index] != requiredBit }
+    }
+    check(candidates.size == 1)
+    return candidates.first().toInt(2)
+  }
+
+  private fun mostCommonChar(report: List<String>, index: Int): Char {
+    val zeroes = report.count { it[index] == '0' }
+    val ones = report.count { it[index] == '1' }
+    return if (zeroes > ones) '0' else '1'
+  }
+
+  private fun leastCommonChar(report: List<String>, index: Int): Char {
+    val zeroes = report.count { it[index] == '0' }
+    val ones = report.count { it[index] == '1' }
+    return if (zeroes > ones) '1' else '0'
+  }
+}
