59a5556da4b0d71f63b65ec190035d303ed51f57
Chris Dziemborowicz <chris@dziemborowicz.com>
2022-11-30 22:25:44 -0800

    Add Y2021D2 solution.

---
+++ src/main/kotlin/PuzzleY2021D2.kt
@@ -0,0 +1,40 @@
+class PuzzleY2021D2 : Puzzle {
+
+  private lateinit var commands: List<Pair<String, Int>>
+
+  override fun parse(input: String) {
+    commands = input.parseStringInts()
+  }
+
+  override fun solve1(): Int {
+    var horizontalPosition = 0
+    var depth = 0
+    commands.forEach {
+      when (it.first) {
+        "forward" -> horizontalPosition += it.second
+        "down" -> depth += it.second
+        "up" -> depth -= it.second
+        else -> throw AssertionError()
+      }
+    }
+    return horizontalPosition * depth
+  }
+
+  override fun solve2(): Int {
+    var horizontalPosition = 0
+    var depth = 0
+    var aim = 0
+    commands.forEach {
+      when (it.first) {
+        "forward" -> {
+          horizontalPosition += it.second
+          depth += aim * it.second
+        }
+        "down" -> aim += it.second
+        "up" -> aim -= it.second
+        else -> throw AssertionError()
+      }
+    }
+    return horizontalPosition * depth
+  }
+}
