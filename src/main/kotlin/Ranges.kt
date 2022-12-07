d0cec7f30aaf4b16df033e836144a2aee0ae1656
Chris Dziemborowicz <chris@dziemborowicz.com>
2022-12-06 20:35:03 -0800

    Add Y2021D22 solution.

--- src/main/kotlin/Ranges.kt
+++ src/main/kotlin/Ranges.kt
@@ -1,17 +1,17 @@
 fun String.toIntRange(): IntRange {
-  val parts = split(Regex("\\D+"))
+  val parts = split(Regex("""(\.\.|,|\s+)"""))
   require(parts.size == 2) { "Invalid format for range: $this" }
   return parts[0].toInt()..parts[1].toInt()
 }
 
 fun String.toLongRange(): LongRange {
-  val parts = split(Regex("\\D+"))
+  val parts = split(Regex("""(\.\.|,|\s+)"""))
   require(parts.size == 2) { "Invalid format for range: $this" }
   return parts[0].toLong()..parts[1].toLong()
 }
 
 fun String.toDoubleRange(): ClosedFloatingPointRange<Double> {
-  val parts = split(Regex("\\D+"))
+  val parts = split(Regex("""(\.\.|,|\s+)"""))
   require(parts.size == 2) { "Invalid format for range: $this" }
   return parts[0].toDouble()..parts[1].toDouble()
 }
