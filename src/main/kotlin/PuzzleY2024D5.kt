d9a4d97cd05108fdd4f66070f41bff758c2f955c
Chris Dziemborowicz <chris@dziemborowicz.com>
2024-12-06 20:43:06 -0800

    Improve Y2024D5 solution.

--- src/main/kotlin/PuzzleY2024D5.kt
+++ src/main/kotlin/PuzzleY2024D5.kt
@@ -1,35 +1,22 @@
 class PuzzleY2024D5 : Puzzle {
 
-  private lateinit var pagesBefore: Map<Int, List<Int>>
+  private lateinit var orderingRules: Map<Pair<Int, Int>, Int>
   private lateinit var manuals: List<List<Int>>
 
   override fun parse(input: String) {
     val (orderingRules, manuals) = input.lines().splitByBlank().map { it.map { it.extractInts() } }
-    this.pagesBefore = orderingRules.groupBy({ it[1] }, { it[0] })
+    this.orderingRules =
+      orderingRules.flatMap { listOf(it.toPair() to -1, it.reversed().toPair() to 1) }.toMap()
     this.manuals = manuals
   }
 
-  override fun solve1(): Int = manuals.filter { it.isValid() }.sumOf { it.middle() }
+  override fun solve1(): Int = manuals.filter { it == it.reordered() }.sumOf { it.middle() }
 
-  override fun solve2(): Int = manuals.filter { !it.isValid() }.sumOf { it.reordered().middle() }
+  override fun solve2(): Int =
+    manuals.filter { it != it.reordered() }.sumOf { it.reordered().middle() }
 
-  private fun List<Int>.isValid(): Boolean {
-    return indices.all { index -> pagesBefore[this[index]]?.all { indexOf(it) < index } ?: true }
-  }
-
-  private fun List<Int>.reordered(): List<Int> {
-    val list = toMutableList()
-    var i = 0
-    while (i in list.indices) {
-      val j = pagesBefore[list[i]]?.maxOf { list.indexOf(it) } ?: -1
-      if (j > i) {
-        list.add(j, list.removeAt(i))
-      } else {
-        i++
-      }
-    }
-    return list
-  }
+  private fun List<Int>.reordered(): List<Int> =
+    sortedWith { a, b -> orderingRules.getValue(a to b) }
 
   companion object {
     val testInput1 = """
