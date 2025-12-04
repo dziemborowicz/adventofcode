a44963be9f9a3a9c294fc36e81bb735db8f2312d
Chris Dziemborowicz <chris@dziemborowicz.com>
2025-12-04 13:17:19 -0800

    Add Y2019D21 solution.

---
+++ src/main/kotlin/PuzzleY2019D21.kt
@@ -0,0 +1,175 @@
+class PuzzleY2019D21 : Puzzle {
+
+  private class Memory(values: List<Long> = listOf()) {
+    private val data = values.mapIndexed { index, value -> index.toLong() to value }.toHashMap()
+
+    operator fun get(index: Long): Long {
+      return data.getOrDefault(index, 0)
+    }
+
+    operator fun set(index: Long, value: Long) {
+      data[index] = value
+    }
+  }
+
+  private lateinit var initialMemory: List<Long>
+
+  override fun parse(input: String) {
+    initialMemory = input.extractLongs()
+  }
+
+  override fun solve1(): Long {
+    val input = """
+      NOT C J
+      AND D J
+      NOT A T
+      OR T J
+      WALK
+
+      """.trimIndent().map { it.code.toLong() }.toDeque()
+
+    var damage = 0L
+    execute(Memory(initialMemory), input = { input.removeFirst() }, output = { damage = it })
+    return damage
+  }
+
+  override fun solve2(): Long {
+    val input = """
+      NOT C J
+      AND D J
+      AND H J
+      NOT B T
+      AND D T
+      OR T J
+      NOT A T
+      OR T J
+      RUN
+
+      """.trimIndent().map { it.code.toLong() }.toDeque()
+
+    var damage = 0L
+    execute(Memory(initialMemory), input = { input.removeFirst() }, output = { damage = it })
+    return damage
+  }
+
+  companion object {
+    private fun execute(memory: Memory, input: () -> Long, output: (Long) -> Unit): Long {
+      var ip = 0L
+      var relativeBase = 0L
+
+      fun a(): Long {
+        return when ((memory[ip] / 100) % 10) {
+          0L -> memory[memory[ip + 1]]
+          1L -> memory[ip + 1]
+          2L -> memory[memory[ip + 1] + relativeBase]
+          else -> fail()
+        }
+      }
+
+      fun a(value: Long) {
+        when ((memory[ip] / 100) % 10) {
+          0L -> memory[memory[ip + 1]] = value
+          1L -> fail("Cannot write memory in immediate mode.")
+          2L -> memory[memory[ip + 1] + relativeBase] = value
+          else -> fail()
+        }
+      }
+
+      fun b(): Long {
+        return when ((memory[ip] / 1000) % 10) {
+          0L -> memory[memory[ip + 2]]
+          1L -> memory[ip + 2]
+          2L -> memory[memory[ip + 2] + relativeBase]
+          else -> fail()
+        }
+      }
+
+      fun b(value: Long) {
+        when ((memory[ip] / 1000) % 10) {
+          0L -> memory[memory[ip + 2]] = value
+          1L -> fail("Cannot write memory in immediate mode.")
+          2L -> memory[memory[ip + 2] + relativeBase] = value
+          else -> fail()
+        }
+      }
+
+      fun c(value: Long) {
+        when ((memory[ip] / 10000) % 10) {
+          0L -> memory[memory[ip + 3]] = value
+          1L -> fail("Cannot write memory in immediate mode.")
+          2L -> memory[memory[ip + 3] + relativeBase] = value
+          else -> fail()
+        }
+      }
+
+      while (true) {
+        when (memory[ip] % 100) {
+          // Add
+          1L -> {
+            c(a() + b())
+            ip += 4
+          }
+
+          // Multiply
+          2L -> {
+            c(a() * b())
+            ip += 4
+          }
+
+          // Input
+          3L -> {
+            a(input())
+            ip += 2
+          }
+
+          // Output
+          4L -> {
+            output(a())
+            ip += 2
+          }
+
+          // Jump-if-true
+          5L -> {
+            if (a() != 0L) {
+              ip = b()
+            } else {
+              ip += 3
+            }
+          }
+
+          // Jump-if-false
+          6L -> {
+            if (a() == 0L) {
+              ip = b()
+            } else {
+              ip += 3
+            }
+          }
+
+          // Less than
+          7L -> {
+            c(if (a() < b()) 1 else 0)
+            ip += 4
+          }
+
+          // Equals
+          8L -> {
+            c(if (a() == b()) 1 else 0)
+            ip += 4
+          }
+
+          // Adjust relative base
+          9L -> {
+            relativeBase += a()
+            ip += 2
+          }
+
+          // Halt
+          99L -> return memory[0]
+
+          else -> fail()
+        }
+      }
+    }
+  }
+}
